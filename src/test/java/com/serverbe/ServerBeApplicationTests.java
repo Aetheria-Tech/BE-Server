9e7ab32f4b93da6428109fd4df0c595ed54c29a8
Duskafka <rlarlgus0206@naver.com>
2025-12-14 22:41:52 +0900

    init

---
+++ src/test/java/com/serverbe/ServerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
