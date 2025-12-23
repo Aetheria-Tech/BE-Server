087414aa61c3050a48dfcf7933a81ba64b4788bd
Duskafka <rlarlgus0206@naver.com>
2025-12-23 12:21:02 +0900

    refactor(art): deleteRunningArt 메서드 파라미터명 `runningArtId`로 변경 (11)
    runningArtid를 runningArtId로 변경하여 카멜 케이스 명명 규칙을 준수합니다.
    이는 내부 일관성을 개선하며 코드 가독성을 높입니다.

--- src/main/java/com/serverbe/application/port/in/art/ManageRunningArtUseCase.java
+++ src/main/java/com/serverbe/application/port/in/art/ManageRunningArtUseCase.java
@@ -4,10 +4,10 @@ import com.serverbe.application.port.in.dto.art.UpdateRunningArtCommand;
 
 public interface ManageRunningArtUseCase {
     // 3. 런닝 아트 ID로 런닝 아트 삭제
-    void deleteRunningArt(Long userId, Long runningArtid);
+    void deleteRunningArt(Long userId, Long runningArtId);
 
     // 4. 런닝 아트 ID로 런닝 아트 수정
-    void updateRunningArt(Long userId, Long runningAryId, UpdateRunningArtCommand command);
+    void updateRunningArt(Long userId, Long runningArtId, UpdateRunningArtCommand command);
 
     // 5. 사용자 ID로 런닝 아트 삭제
     void deleteAllRunningArtsByUserId(Long userId);
