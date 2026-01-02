79cc5bba19fcb234a0bab5d615680e8c1658f0c2
Duskafka <rlarlgus0206@naver.com>
2026-01-02 17:26:43 +0900

    refactor(logging): 로그 포멧 메소드 구현 (#20)
    formatLogPrefix 메소드를 구현하여 각 로깅에 중복되는 코드를 최소화했습니다.

--- src/main/java/com/serverbe/infrastructure/common/logging/TraceAspect.java
+++ src/main/java/com/serverbe/infrastructure/common/logging/TraceAspect.java
@@ -13,37 +13,28 @@ import java.util.Arrays;
 @Component
 public class TraceAspect {
 
-    // 정렬을 위한 너비 설정 (이것은 스타일이 아니라 데이터 포맷이므로 Java에 두어도 무방합니다)
-    private static final int TAG_WIDTH = 11;      // [EXCEPTION] 길이에 맞춤
-    private static final int LOCATION_WIDTH = 45; // 클래스.메서드명 너비
+    private final int tagWidth = 11;      // [EXCEPTION] 길이에 맞춤
+    private final int locationWidth = 45; // 클래스.메서드명 너비
 
     @Around("@within(com.serverbe.infrastructure.common.logging.Trace)")
     public Object logTrace(ProceedingJoinPoint joinPoint) throws Throwable {
-        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
-        String methodName = joinPoint.getSignature().getName();
-        String location = className + "." + methodName;
+        String location = joinPoint.getSignature().getDeclaringType().getSimpleName() + "." + joinPoint.getSignature().getName();
 
-        // [ENTRY] 출력: 색상 코드 제거, 순수 텍스트 정렬만 수행
-        String entryTag = String.format("%-" + TAG_WIDTH + "s", "[ENTRY]");
-        String entryLocation = String.format("%-" + LOCATION_WIDTH + "s", location);
-        log.info("{} {}| Args: {}", entryTag, entryLocation, Arrays.toString(joinPoint.getArgs()));
+        log.info("{}| Args: {}", formatLogPrefix("[ENTRY]", location), Arrays.toString(joinPoint.getArgs()));
 
         try {
             Object result = joinPoint.proceed();
-
-            // [EXIT] 출력
-            String exitTag = String.format("%-" + TAG_WIDTH + "s", "[EXIT]");
-            String exitLocation = String.format("%-" + LOCATION_WIDTH + "s", location);
-            log.info("{} {}| Return: {}", exitTag, exitLocation, result);
-
+            log.info("{}| Return: {}", formatLogPrefix("[EXIT]", location), result);
             return result;
         } catch (Throwable e) {
-            // [EXCEPTION] 출력
-            String exTag = String.format("%-" + TAG_WIDTH + "s", "[EXCEPTION]");
-            String exLocation = String.format("%-" + LOCATION_WIDTH + "s", location);
-            log.error("{} {}| Exception: {}", exTag, exLocation, e.getClass().getSimpleName());
-
+            log.error("{}| Exception: {}", formatLogPrefix("[EXCEPTION]", location), e.getClass().getSimpleName());
             throw e;
         }
+    }
+
+    private String formatLogPrefix(String tag, String location) {
+        String formattedTag = String.format("%-" + tagWidth + "s", tag);
+        String formattedLocation = String.format("%-" + locationWidth + "s", location);
+        return formattedTag + " " + formattedLocation;
     }
 }
